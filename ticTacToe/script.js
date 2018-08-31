955c66bf60446777dac2209eac028b9b00a883bf
Linda Wang <lindawang256@gmail.com>
2018-08-31 15:16:21 -0700

    Add minimax function

--- ticTacToe/script.js
+++ ticTacToe/script.js
@@ -13,7 +13,7 @@ const winCombos = [
 ]
 
 const cells = document.querySelectorAll('.cell');
-console.log(cells);
+//console.log(cells);
 startGame();
 
 function startGame() {
@@ -25,11 +25,13 @@ function startGame() {
     cells[i].style.removeProperty('background-color');
     cells[i].addEventListener('click',turnClick,false)
   }
-
+}
   function turnClick(square){
+    if (typeof originalBoard[square.target.id] == 'number'){
     turn(square.target.id, huPlayer);
+    if(!checkTie()) turn(bestSpot(),machinePlayer);
+    }
   }
-
 
   function turn(squareId, player){ //player is current player
     originalBoard[squareId]=player;
@@ -38,6 +40,35 @@ function startGame() {
     let gameWon = checkWin(originalBoard,player);
     if (gameWon) gameOver(gameWon);
   }
+
+ function declareWinner(who){
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText =who;
+
+ }
+
+  function emptySquares(){
+    return originalBoard.filter( s=> typeof s ==='number')
+  }
+
+  function bestSpot(){
+    //return emptySquares()[0];
+    return minimax(originalBoard,machinePlayer).index;
+  }
+
+  function checkTie(){
+    //if no empty squares and nobody won(checkWin function checking winner for every move), it is a tie.
+    if (emptySquares().length === 0){
+      for (let i=0; i< cells.length; i++){
+        cells[i].style.backgroundColor = 'green';
+        cells[i].removeEventListener('click', turnClick,false);
+      }
+      declareWinner("Tie Game");
+      return true;
+    }
+    return false;
+  }
+
 
   function checkWin(board,player){
     //find all the places on the board that already been played in.
@@ -60,5 +91,56 @@ function startGame() {
     for (let i=0; i<cells.length; i++){
       cells[i].removeEventListener('click',turnClick,false);
     }
+    declareWinner(gameWon.player === huPlayer ? "You win!" : "You lose!");
   }
+
+  function minimax(newBoard,player){
+    var availSpots = emptySquares(newBoard);
+
+    if (checkWin(newBoard,player)){
+      return {score: -10};
+    }else if (checkWin(newBoard,machinePlayer)){
+      return {score: 10};
+    }else if (availSpots.length===0){
+      return {score:0};
+    }
+
+    var moves =[];
+    for (var i=0; i< availSpots.length; i++){
+      var move ={};
+      move.index = newBoard[availSpots[i]];
+      newBoard[availSpots[i]] = player;
+
+      if (player === machinePlayer) {
+        var result = minimax(newBoard, huPlayer);
+        move.score = result.score;
+      } else {
+        var result = minimax(newBoard, machinePlayer);
+        move.score = result.score;
+      }
+      newBoard[availSpots[i]] = move.index;
+        moves.push(move);
+      }
+
+      var bestMove;
+      if(player === machinePlayer){
+        var bestScore = -10000;
+        for (var i=0; i<moves.length; i++){
+          if (moves[i].score > bestScore){
+            bestScore = moves[i].score;
+            bestMove = i;
+          }
+        }
+
+      } else {
+        var bestScore = 10000;
+        for (var i=0; i< moves.length; i++){
+          if (moves[i].score < bestScore){
+            bestScore = moves[i].score;
+            bestMove = i;
+          }
+        }
+      }
+    return moves[bestMove];
+
 }
