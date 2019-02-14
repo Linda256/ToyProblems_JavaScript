19a4aea3f8479422101cb20c48663c4abd21bc38
Linda Wang <lindawang256@gmail.com>
2019-02-14 09:14:46 -0800

    Add customized match rules for balanced parentheses

--- HRR/balancedParentheses.js
+++ HRR/balancedParentheses.js
@@ -26,27 +26,64 @@
  I: String [](){}
  O: boolean
  */
- var balancedParens = function(input) {
-  let left = ["(","{","["];
-  let right =[")","}","]"];
+ // var balancedParens = function(input) {
+ //  let left = ["(","{","["];
+ //  let right =[")","}","]"];
+ //  let stack=[];
+ //  for (let i=0;i<input.length;i++){
+ //    let index=left.indexOf(input[i])
+ //    if(index>=0){
+ //      stack.push(right[index]);
+ //    }
+ //    if(right.includes(input[i])){
+ //      let paren=stack.pop();
+ //      if(paren!==input[i]){
+ //        return false;
+ //      }
+ //    }
+ //  }
+ //  return stack.length===0;
+ // }
+
+ // //let input ='[](){}';
+ // //let input=' var wow  = { yo: thisIsAwesome() }';
+ // let input ='var hubble = function() { telescopes.awesome()';
+ // console.log(balancedParens(input));
+
+
+// pass in a rules object to customise balance
+
+
+var advancedSolution = function(input, rules) {
   let stack=[];
-  for (let i=0;i<input.length;i++){
-    let index=left.indexOf(input[i])
-    if(index>=0){
-      stack.push(right[index]);
-    }
-    if(right.includes(input[i])){
-      let paren=stack.pop();
-      if(paren!==input[i]){
-        return false;
-      }
+  rules = rules || {
+    '[': ']',
+    '(': ')',
+    '{': '}'
+  };
+  // create a filter object based on the given or default rule set
+  var startChars = Object.keys(rules);
+  var filterChars = startChars.reduce(function(m, i) {
+    return m + i + rules[i];
+  }, '');
+
+  for ( var i = 0; i < input.length; i++ ) {
+    var c = input[i];
+
+    if ( filterChars.indexOf(c) < 0 ) {
+      continue;
+    } else if ( startChars.indexOf(c) >= 0 ) {
+      stack.push(c);
+    } else if ( c !== rules[stack.pop()] ) {
+      return false;
     }
   }
-  return stack.length===0;
- }
+
+  return stack.length === 0;
+};
+
 
  //let input ='[](){}';
- //let input=' var wow  = { yo: thisIsAwesome() }';
- let input ='var hubble = function() { telescopes.awesome()';
- console.log(balancedParens(input));
-
+ let input=' var wow  = { yo: thisIsAwesome() }';
+ //let input ='var hubble = function() { telescopes.awesome()';
+ console.log(advancedSolution(input));
